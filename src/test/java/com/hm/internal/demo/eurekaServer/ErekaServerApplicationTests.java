c5ba82739dfa4d9d6119eb9e63be33aae9ca9870
Srinivasan Thoyyeti <srinivasan.thoyyeti@happiestminds.com>
2021-01-20 10:06:16 +0530

    intit version

---
+++ src/test/java/com/hm/internal/demo/eurekaServer/ErekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.internal.demo.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
